8b217d4ed196d273eb6e6c8f186034ca32eb04e8
Nga <hocmetmuonchet@gmail.com>
2019-05-13 18:42:22 +0700

    Chỉnh sửa

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,16 +5,16 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Laravel </title>
 	<base href="{{asset('')}}">
-	<link href='http://fonts.googleapis.com/css?family=Dosis:300,400' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+	<link href='source/assets/dest/css/font.css' rel='stylesheet' type='text/css'>
+	<link href='source/assets/dest/css/font1.css' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="source/assets/dest/css/bootstrap.min.css">
 	<link rel="stylesheet" href="source/assets/dest/css/font-awesome.min.css">
 	<link rel="stylesheet" href="source/assets/dest/vendors/colorbox/example3/colorbox.css">
 	<link rel="stylesheet" href="source/assets/dest/rs-plugin/css/settings.css">
 	<link rel="stylesheet" href="source/assets/dest/rs-plugin/css/responsive.css">
 	<link rel="stylesheet" title="style" href="source/assets/dest/css/style.css">
 	<link rel="stylesheet" href="source/assets/dest/css/animate.css">
-	<link rel="stylesheet" title="style" href="source/assets/dest/css/huong-style.css">
+	<link rel="stylesheet" title="style" href="source/assets/dest/css/style1.css">
 </head>
 <body>
 
@@ -28,7 +28,7 @@
 	<!-- include js files -->
 	<script src="source/assets/dest/js/jquery.js"></script>
 	<script src="source/assets/dest/vendors/jqueryui/jquery-ui-1.10.4.custom.min.js"></script>
-	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+	<script src="source/assets/dest/js/bootstrap.min.js"></script>
 	<script src="source/assets/dest/vendors/bxslider/jquery.bxslider.min.js"></script>
 	<script src="source/assets/dest/vendors/colorbox/jquery.colorbox-min.js"></script>
 	<script src="source/assets/dest/vendors/animo/Animo.js"></script>
